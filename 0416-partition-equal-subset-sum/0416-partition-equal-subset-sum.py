9aa8bd1d244623734040a66980c47fecd477a643
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-09 11:37:07 -0400

    Time: 2215 ms (36.14%), Space: 32.2 MB (40.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
         sum_ = sum(nums)
         if sum_ % 2 != 0:
             return False
-        sum_ = sum_/2
+        sum_ = sum_//2
         n = len(nums)
 
+        """
+        Brute force + memoization
         @lru_cache(maxsize = None)
         def dfs(i, nums, sum_):
             if sum_ == 0:
@@ -18,4 +19,18 @@ class Solution:
             result = dfs(i - 1, nums, sum_) or dfs(i - 1, nums, sum_ - nums[i - 1])
             return result
 
-        return dfs(n - 1, tuple(nums), sum_)
+        return dfs(n - 1, tuple(nums), sum_)        
+        """
+        #dynamic programming
+        dp = [[False] * (sum_ + 1) for i in range(n + 1)]
+        dp[0][0] = True
+
+        for i in range(1, n + 1):
+            cur = nums[i - 1]
+            for j in range(sum_ + 1):
+                if j < cur:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - cur]
+        
+        return dp[n][sum_]
