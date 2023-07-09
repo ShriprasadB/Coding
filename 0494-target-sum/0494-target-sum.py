ca907b945974a81dc82cf6632384576e9f9a8f63
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-09 14:55:23 -0400

    Time: 371 ms (34.68%), Space: 37.9 MB (23.8%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        total = sum(nums)
-        dp = [[-1]*(2*total + 1) for i in range(n)]
         
+        '''
+        memoization
+        
+        dp = [[-1]*(2*total + 1) for i in range(n)]
         def dfs(i, sum_):
             if i == len(nums):
                 if sum_ == target:
@@ -16,4 +17,23 @@ class Solution:
             dp[i][total + sum_] = dfs(i + 1, sum_ + nums[i]) + dfs(i + 1, sum_ - nums[i])
             return dp[i][total + sum_]
     
-        return dfs(0, 0) 
+        return dfs(0, 0)
+        '''
+        n = len(nums)
+        dp = {}
+
+        def dfs(i, sum_):
+            if i == n:
+                if sum_ == target:
+                    return 1
+                return 0
+            
+            if (i, sum_) in dp:
+                return dp[(i, sum_)]
+            
+            dp[(i, sum_)] = dfs(i + 1, sum_ + nums[i]) + dfs(i + 1, sum_ - nums[i])
+            return dp[(i, sum_)]
+
+        return dfs(0, 0)
+
+
