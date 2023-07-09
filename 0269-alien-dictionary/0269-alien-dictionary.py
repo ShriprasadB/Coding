ecbb682ccf50f82582214509178216c1552e13ce
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-08 22:04:34 -0400

    Time: 51 ms (58.26%), Space: 16.4 MB (72.6%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,7 +1,8 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {c:set() for w in words for c in w}
+        adj = {c:[] for w in words for c in w}
         n = len(words)
+        indegree = collections.defaultdict(int)
 
         for i in range(1, n):
             w1, w2 = words[i - 1], words[i]
@@ -12,32 +13,23 @@ class Solution:
             
             for j in range(min_len):
                 if w1[j] != w2[j]:
-                    adj[w1[j]].add(w2[j])
+                    adj[w1[j]].append(w2[j])
+                    indegree[w2[j]] += 1
                     break
-        
-        answer = []
-        visit = set()
-        cycle = set()
-        def dfs(c):
-            if c in cycle:
-                return False
             
-            if c in visit:
-                return True
-            
-            cycle.add(c)
-
-            for nei in adj[c]:
-                if not dfs(nei):
-                    return False
-            
-            cycle.remove(c)
-            visit.add(c)
-            answer.append(c)
-            return True
-
+        q = []
         for c in adj:
-            if not dfs(c):
-                return ""
+            if not indegree[c]:
+                q.append(c)
+
+        answer = []
+        while q:
+            node = q.pop(0)
+            answer.append(node)
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
         
-        return "".join(answer)[::-1]
+        return "".join(answer) if len(answer) == len(adj) else ""
+        
