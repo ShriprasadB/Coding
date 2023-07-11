64a942a93f81061e27722cd8059132a22e278994
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-10 21:17:52 -0400

    Time: 1351 ms (63.6%), Space: 37.3 MB (15.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}
+        @lru_cache(None)
         def dfs(amount):
             if amount < 0:
                 return -1
             if amount == 0:
                 return 0
-            
-            if amount in dp:
-                return dp[amount]
             
             min_cost = float("inf")
             for coin in coins:
@@ -16,7 +13,6 @@ class Solution:
                 if res != -1:
                     min_cost = min(min_cost, res + 1)
             
-            dp[amount] = min_cost if min_cost != float('inf') else -1
-            return dp[amount]
+            return min_cost if min_cost != float('inf') else -1
 
         return dfs(amount)
