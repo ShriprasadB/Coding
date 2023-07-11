8ce10f3ed277bf87f6c7ee94a881e47ede948d26
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-11 11:48:39 -0400

    Time: 71 ms (25.47%), Space: 16.2 MB (94.88%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,7 +1,7 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         dp = {}
-
+        '''
         def dfs(i):
             if i == len(days):
                 return 0
@@ -19,3 +19,16 @@ class Solution:
             return dp[i]
 
         return dfs(0)
+        '''
+
+        for i in range(len(days) - 1, -1, -1):
+            dp[i] = float('inf')
+            for d, c in zip([1, 7, 30], costs):
+                j = i
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dp.get(j, 0))
+            
+        return dp[0]
+
+
