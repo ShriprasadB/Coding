cd8d3753cd4c237dee9bda4980807232d5ee672b
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-12 22:48:43 -0400

    Time: 117 ms (50.46%), Space: 18.1 MB (68.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,13 +2,13 @@ class Solution:
     def canFinish(self, numCourses, prerequisites):
         indegree = [0]*numCourses
         adj = collections.defaultdict(list)
-
+        
         for u, v in prerequisites:
             adj[u].append(v)
             indegree[v] += 1
         
         q = []
-        visited = 0
+        visited = set()
 
         for i in range(numCourses):
             if not indegree[i]:
@@ -16,10 +16,11 @@ class Solution:
 
         while q:
             node = q.pop(0)
-            visited += 1
+            visited.add(node)
             for nei in adj[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
+                if nei not in visited:
+                    indegree[nei] -= 1
+                    if indegree[nei] == 0:
+                        q.append(nei)
             
-        return visited == numCourses
+        return len(visited) == numCourses
