d93c827555fb76d3490b16911ac1a3ab6ba4f491
ShriprasadB <shree.bhamare1211@gmail.com>
2023-07-25 21:15:34 -0400

    Time: 6065 ms (7.69%), Space: 30.9 MB (25.64%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        left = 1
+        right = 1000000000
+        min_speed = float('inf')
+
+        while left <= right:
+            mid = (left + right)//2
+
+            total = 0
+            for i, d in enumerate(dist):
+                if i == len(dist) - 1:
+                    total += d/mid
+                else:
+                    total += ceil(d/mid)
+                
+            if total > hour:
+                left = mid + 1
+            else:
+                right = mid - 1
+                min_speed = min(min_speed, mid)
+
+        return min_speed if min_speed != float('inf') else -1
