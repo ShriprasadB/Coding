bbed91861174077475491fd7625b4aca7672a80e
ShriprasadB <shree.bhamare1211@gmail.com>
2023-08-01 09:50:17 -0400

    Time: 61 ms (71.33%), Space: 45.9 MB (5.99%) - LeetHub

---
+++ 2798-chunk-array/2798-chunk-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    const result = [];
+    let currentChunk = [];
+
+    for (const element of arr){
+        if (currentChunk.length == size){
+            result.push(currentChunk);
+            currentChunk = [];
+        }
+        currentChunk.push(element);
+    }
+
+    if (currentChunk.length) result.push(currentChunk);
+    return result;
+};
