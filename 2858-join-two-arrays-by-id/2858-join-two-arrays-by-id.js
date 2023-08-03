31f4802e2e8fc51c88df5c3b5b17ac8f0ab2d0ed
ShriprasadB <shree.bhamare1211@gmail.com>
2023-08-03 11:20:17 -0400

    Time: 302 ms (93.61%), Space: 122.2 MB (17.37%) - LeetHub

---
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    const combine = arr1.concat(arr2);
+    const merged = {};
+
+    combine.forEach((obj) => {
+        const id = obj.id;
+        if (!merged[id]){
+            merged[id] = { ...obj };
+        }
+        else{
+            merged[id] = { ...merged[id], ...obj };
+        }
+    });
+
+    const joined = Object.values(merged);
+    joined.sort((a,b) => a.id - b.id);
+
+    return joined;
+};
