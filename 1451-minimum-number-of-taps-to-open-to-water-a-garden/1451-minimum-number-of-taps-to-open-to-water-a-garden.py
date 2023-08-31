0a3203ecc43f5c76a4bc7328d472d16e12f0aacf
ShriprasadB <shree.bhamare1211@gmail.com>
2023-08-31 13:38:10 -0400

    Time: 122 ms (89.29%), Space: 16.9 MB (46.9%) - LeetHub

---
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        max_reach = [0] * (n + 1)
+
+        for i in range(len(ranges)):
+            start = max(0, i - ranges[i])
+            end = min(n, i + ranges[i])
+
+            max_reach[start] = max(max_reach[start], end)
+        
+        
+        taps, curr_end, next_end  = 0, 0, 0
+        
+        for i in range(n + 1):
+            if i > next_end:
+                return -1
+
+            if i > curr_end:
+                taps += 1
+                curr_end = next_end
+
+            next_end = max(next_end, max_reach[i])
+
+        return taps
