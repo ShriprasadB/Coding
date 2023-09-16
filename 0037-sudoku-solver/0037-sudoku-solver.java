80a02b8e4193e9e17d27abc7009f121edb5b76a5
ShriprasadB <shree.bhamare1211@gmail.com>
2023-09-15 23:30:15 -0400

    Time: 9 ms (58.4%), Space: 39.8 MB (64.04%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,102 +1,67 @@
 class Solution {
-  // box size
-  int n = 3;
-  // row size
-  int N = n * n;
-
-  int [][] rows = new int[N][N + 1];
-  int [][] columns = new int[N][N + 1];
-  int [][] boxes = new int[N][N + 1];
-
-  char[][] board;
-
-  boolean sudokuSolved = false;
-
-  public boolean couldPlace(int d, int row, int col) {
-    /*
-    Check if one could place a number d in (row, col) cell
-    */
-    int idx = (row / n ) * n + col / n;
-    return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
-  }
-
-  public void placeNumber(int d, int row, int col) {
-    /*
-    Place a number d in (row, col) cell
-    */
-    int idx = (row / n ) * n + col / n;
-
-    rows[row][d]++;
-    columns[col][d]++;
-    boxes[idx][d]++;
-    board[row][col] = (char)(d + '0');
-  }
-
-  public void removeNumber(int d, int row, int col) {
-    /*
-    Remove a number which didn't lead to a solution
-    */
-    int idx = (row / n ) * n + col / n;
-    rows[row][d]--;
-    columns[col][d]--;
-    boxes[idx][d]--;
-    board[row][col] = '.';
-  }
-
-  public void placeNextNumbers(int row, int col) {
-    /*
-    Call backtrack function in recursion
-    to continue to place numbers
-    till the moment we have a solution
-    */
-    // if we're in the last cell
-    // that means we have the solution
-    if ((col == N - 1) && (row == N - 1)) {
-      sudokuSolved = true;
+    public void solveSudoku(char[][] board) {
+        // Start solving sudoku from the first cell
+        solve(board, 0, 0);
     }
-    // if not yet
-    else {
-      // if we're in the end of the row
-      // go to the next row
-      if (col == N - 1) backtrack(row + 1, 0);
-        // go to the next column
-      else backtrack(row, col + 1);
-    }
-  }
 
-  public void backtrack(int row, int col) {
-    /*
-    Backtracking
-    */
-    // if the cell is empty
-    if (board[row][col] == '.') {
-      // iterate over all numbers from 1 to 9
-      for (int d = 1; d < 10; d++) {
-        if (couldPlace(d, row, col)) {
-          placeNumber(d, row, col);
-          placeNextNumbers(row, col);
-          // if sudoku is solved, there is no need to backtrack
-          // since the single unique solution is promised
-          if (!sudokuSolved) removeNumber(d, row, col);
+    private boolean solve(char[][] board, int row, int col) {
+        // Base case: If row is equal to board length, entire board has been filled
+        if (row == board.length) {
+            return true;
         }
-      }
-    }
-    else placeNextNumbers(row, col);
-  }
-
-  public void solveSudoku(char[][] board) {
-    this.board = board;
-
-    // init rows, columns and boxes
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        char num = board[i][j];
-        if (num != '.') {
-          int d = Character.getNumericValue(num);
-          placeNumber(d, i, j);
+        
+        // Move to next row when current row is fully filled
+        if (col == board[0].length) {
+            return solve(board, row + 1, 0);
         }
-      }
+        
+        // Skip cells that are already filled
+        if (board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
+        
+        // Try different numbers in current cell
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValidPlacement(board, row, col, num)) {
+                board[row][col] = num; // Fill current cell with valid number
+                
+                // Move to next cell
+                if (solve(board, row, col + 1)) {
+                    return true;
+                }
+                
+                // Backtrack to previous state if solution not found
+                board[row][col] = '.';
+            }
+        }
+        
+        // No valid solution found
+        return false;
     }
-    backtrack(0, 0);
-  }
+
+    private boolean isValidPlacement(char[][] board, int row, int col, char num) {
+        // Check if num is already in the same row, column or 3x3 subgrid
+        for (int i = 0; i < board.length; i++) {
+            // Check row
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check column
+            if (board[row][i] == num) {
+                return false;
+            }
+ 
+            // Check 3x3 subgrid
+            int subgridRow = 3 * (row / 3) + i / 3; // Calculate row index of subgrid
+            int subgridCol = 3 * (col / 3) + i % 3; // Calculate column index of subgrid
+ 
+            if (board[subgridRow][subgridCol] == num) {
+                return false;
+            }
+        }
+
+        // Placement is valid
+        return true;
+    }
 }
